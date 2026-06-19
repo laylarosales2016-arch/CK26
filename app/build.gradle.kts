ac94d2b690ec7ba3acc186877a2c1989ea7278b2
CK <laylarosales2016@gmail.com>
2026-06-20 01:12:48 +0800

    Enable automatic APK signing with secrets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sam.myapplication"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "11"
+        versionCode = 14
+        versionName = "14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
