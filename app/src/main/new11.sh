8b4380f0c3c02f1160d4664749b2744702d68667
CK <laylarosales2016@gmail.com>
2026-06-25 23:51:30 +0800

    Trigger signed build v17

--- app/src/main/new11.sh
+++ app/src/main/new11.sh
@@ -1,4 +1,9 @@
-    git add .
-    git commit -m "Secrets added, trigger signed build"
-    git push origin master
+# 1. Update version to 17
+sed -i 's/versionCode = 16/versionCode = 17/' app/build.gradle.kts
+sed -i 's/versionName = "16"/versionName = "17"/' app/build.gradle.kts
+
+# 2. Add, Commit and Push (Build will NOT skip)
+git add .
+git commit -m "Trigger signed build v17"
+git push origin master
     
