da0e0ead54414d9ab06926b46e176e5bb2d5d4d3
CK <laylarosales2016@gmail.com>
2026-06-29 02:45:49 +0800

    Ensure Overbreak and Late of 5 minutes or more count as Gold Medals [skip ci]

--- app/src/main/new11.sh
+++ app/src/main/new11.sh
@@ -1,8 +1,8 @@
 # 1. Update version from 17 to 18
-sed -i 's/versionCode = 17/versionCode = 18/' app/build.gradle.kts
-sed -i 's/versionName = "17"/versionName = "18"/' app/build.gradle.kts
+sed -i 's/versionCode = 18/versionCode = 19/' app/build.gradle.kts
+sed -i 's/versionName = "18"/versionName = "19"/' app/build.gradle.kts
 
 # 2. Add, Commit and Push (Build will NOT skip)
 git add .
-git commit -m "Trigger signed build v18"
+git commit -m "Trigger signed build v19"
 git push origin master
