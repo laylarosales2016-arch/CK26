e553ed81b572e5e0e8c90a0f34979c9030db04b2
GitHub Action <action@github.com>
2026-08-02 13:17:12 +0000

    Auto-increment version to 33 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sam.myapplication"
         minSdk = 24
         targetSdk = 36
-        versionCode = 32
-        versionName = "32"
+        versionCode = 33
+        versionName = "33"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
